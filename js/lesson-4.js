6561e94dd7b7e4874ac9b0eba46fbc23925b8777
Svitlana Shumal <svetlankachajka@gmail.com>
2025-05-25 15:32:25 +0300

    added code

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,47 +1,102 @@
 // 1.
 // 1 - отримай body елемент і виведи його в консоль;
+const body = document.querySelector("body");
+console.log(body);
+
 // 2 - отримай елемент id="title" і виведи його в консоль;
+const title = document.querySelector("#title");
+console.log(title);
+
 // 3 - отримай елемент class="list" і виведи його в консоль;
+const list = document.querySelector(".list");
+console.log(list);
+
 // 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
+const dataTopic = document.querySelectorAll("[data-topic]");
+console.log(dataTopic);
+
 // 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const firstElDataTopic = document.querySelectorAll("[data-topic");
+console.log(firstElDataTopic);
+
 // 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const lastElDataTopic = firstElDataTopic[firstElDataTopic.length - 1];
+console.log(lastElDataTopic);
+
 // 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
+const h1El = document.querySelector("h1");
+const nextSibling = h1El.nextElementSibling;
+console.log(nextSibling);
+const prevSibling = h1El.previousElementSibling;
+console.log(prevSibling);
+
 // 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
+const h3Elements = document.querySelectorAll("h3");
+console.log(h3Elements);
+
 // 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка на червоний колір
+const h3El = document.querySelectorAll("h3");
+h3El.forEach((h3) => h3.classList.add("active"));
+console.log(h3El);
+
 // 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+const liTopic = document.querySelector("li[data-topic = navigation]");
+console.log(liTopic);
+
 // 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
+const dataNavig = document.querySelector('[data-topic="navigation"]');
+if (dataNavig) {
+  dataNavig.style.backgroundColor = "yellow";
+}
+console.log(dataNavig);
+
 // 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
+const dataNavigation = document.querySelector('[data-topic="navigation"]');
+console.log(dataNavigation);
+const dataNavPar = dataNavigation.querySelector("p");
+console.log(dataNavPar);
+console.log(dataNavPar.textContent);
+console.log((dataNavPar.textContent = "Я змінив тут текст!"));
+
 // 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
+const currentTopic = "manipulation";
+const currentTopicData = document.querySelector('[data-topic="manipulation"]');
+const currentTopicData2 = document.querySelector(
+  '[data-topic="${currentTopic}"]'
+);
+console.log(currentTopicData);
+console.log(currentTopicData.dataset.topic);
+
 // 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
+const blueColor = document.querySelector('[data-topic="search"]');
+console.log((blueColor.style.backgroundColor = "blue"));
+
 // 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
+const completedEl = document.querySelector(".completed");
+console.log(completedEl);
+
 // 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
+const allLi = document.querySelectorAll("li");
+console.log(allLi);
+console.log(completedEl.parentElement);
+const remEl = completedEl.closest("li").remove();
+console.log(remEl);
+
 // 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
+const elementTitle = document.querySelector("#title");
+console.log(elementTitle);
+const paragraf = document.createElement("p");
+paragraf.textContent = "Об'єктна модель документа (Document Object Model)";
+console.log(paragraf);
+elementTitle.append(paragraf);
+const str = "<p>Об'єктна модель документа (Document Object Model)</p>";
+elementTitle.insertAdjacentHTML("afterend", str);
+const container = document.querySelector(".container");
+console.log(container);
+
 // 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
-
-// const body = document.querySelector("body");
-// // console.log(body);
-// const title = document.querySelector("#title");
-// // console.log(title);
-// const list = document.querySelector(".list");
-// // console.log(list);
-// const dataTopic = document.querySelectorAll("[data-topic]");
-// // console.log(dataTopic);
-// const firstElDataTopic = document.querySelectorAll("[data-topic");
-// // console.log(firstElDataTopic);
-// const lastElDataTopic = firstElDataTopic[firstElDataTopic.length - 1];
-// // console.log(lastElDataTopic);
-// const h1El = document.querySelector("h1");
-// const nextSibling = h1El.nextElementSibling;
-// // console.log(nextSibling);
-// const prevSibling = h1El.previousElementSibling;
-// // console.log(prevSibling);
-// const h3El = document.querySelectorAll("h3");
-// h3El.forEach((h3) => h3.classList.add("active"));
-// console.log(h3El);
-const liTopic = document.querySelector("[data-topic]");
-console.log(liTopic);
 
 // 2.
 // Створіть контейнер div (з класом number-container) в HTML-документі
@@ -51,6 +106,25 @@ console.log(liTopic);
 // Непарні числа - жовтий фон (додати клас odd).
 
 // const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+// const numberContainer = document.getElementById("numberContainer");
+
+// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+// for (let i = 0; i < 100; i++) {
+//   const numberBlock = document.createElement("div");
+//   numberBlock.classList.add("number");
+//   const number = randomNumber();
+//   numberBlock.textContent = number;
+
+//   if (number % 2 === 0) {
+//     numberBlock.classList.add("even");
+//   } else {
+//     numberBlock.classList.add("odd");
+//   }
+
+//   numberContainer.appendChild(numberBlock);
+// }
 
 // 3.
 // Form Events, Input, Focus, Blur and Submit.
